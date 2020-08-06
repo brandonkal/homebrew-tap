347dba4f78a1bddc01277647baefc193dc078729
Brandon Kalinowski <4714862+brandonkal@users.noreply.github.com>
2020-08-05 23:55:43 -0700

    inkjet v0.12.0

--- Formula/inkjet.rb
+++ Formula/inkjet.rb
@@ -1,14 +1,14 @@
 class Inkjet < Formula
   desc "Make your markdown executable with inkjet, the interactive CLI task runner"
   homepage "https://github.com/brandonkal/inkjet"
-  version "0.11.0"
+  version "0.12.0"
 
   if OS.mac?
-    url "https://github.com/brandonkal/inkjet/releases/download/v0.11.0/inkjet-darwin.tar.gz"
-    sha256 "e0cc4938ffa051247ef9ef63ed0bb91e96c4e03f35d3ac4ad28526b1a18db730"
+    url "https://github.com/brandonkal/inkjet/releases/download/v0.12.0/inkjet-darwin.tar.gz"
+    sha256 "fdfe3f06a220700fbdcb1f1427a394cf7d0d089feea6604d31d1f87162d5d870"
   elsif OS.linux?
-    url "https://github.com/brandonkal/inkjet/releases/download/v0.11.0/inkjet-linux.tar.gz"
-    sha256 "8b209e40aed39aac76557d458cd7744af59afe2d278cc3f9e43ec66561d038dc"
+    url "https://github.com/brandonkal/inkjet/releases/download/v0.12.0/inkjet-linux.tar.gz"
+    sha256 "1fb6566c203db2907aa35cd68ee3a7725a53961c26e1b45c29402aaa14274ff7"
   end
 
   depends_on :arch => :x86_64
