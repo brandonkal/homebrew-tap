ea5d0e8a5c84ec4101f852f3bb075e639aa95e63
Brandon Kalinowski <brandon@brandonkalinowski.com>
2024-09-07 13:43:13 -0400

    Release Inkjet v1.0.0

--- Formula/inkjet.rb
+++ Formula/inkjet.rb
@@ -1,17 +1,17 @@
 class Inkjet < Formula
   desc "Make your markdown executable with inkjet, the interactive CLI task runner"
   homepage "https://github.com/brandonkal/inkjet"
-  version "0.15.0"
+  version "1.0.0"
   license = "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/brandonkal/inkjet/releases/download/v0.15.0/inkjet-v0.15.0-aarch64-apple-darwin.tar.gz"
-      sha256 "dd0262a9efba7de1e80921fe5fd900ae5fc67b2d323196b6d21d4d7835232250"
+      url "https://github.com/brandonkal/inkjet/releases/download/v1.0.0/inkjet-v1.0.0-aarch64-apple-darwin.tar.gz"
+      sha256 "9fa7be42e53c5644a56980b98ce8a649e4d2108f513bf7693fdd9b3a2767f41a"
     end
     on_intel do
-      url "https://github.com/brandonkal/inkjet/releases/download/v0.15.0/inkjet-v0.15.0-x86_64-apple-darwin.tar.gz"
-      sha256 "7efe4613c0852c6ff628e723fc9cf8dfcc5143b9c12bcb03818110ad4266b6e1"
+      url "https://github.com/brandonkal/inkjet/releases/download/v1.0.0/inkjet-v1.0.0-x86_64-apple-darwin.tar.gz"
+      sha256 "67f5afe52e26dc4275d73cb1a5efb8113f0739a7e727059cf6692a75ef44ad0c"
     end
   end
 
