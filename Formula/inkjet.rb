27d66d6fa82b30b2ce229f03a0f4139b45f8766b
Brandon Kalinowski <brandon@brandonkalinowski.com>
2024-09-07 03:45:10 -0400

    test inkjet formula

--- Formula/inkjet.rb
+++ Formula/inkjet.rb
@@ -1,20 +1,32 @@
 class Inkjet < Formula
   desc "Make your markdown executable with inkjet, the interactive CLI task runner"
   homepage "https://github.com/brandonkal/inkjet"
-  version "0.12.0"
+  version "0.15.0"
+  license = "MIT"
 
-  if OS.mac?
-    url "https://github.com/brandonkal/inkjet/releases/download/v0.12.0/inkjet-darwin.tar.gz"
-    sha256 "fdfe3f06a220700fbdcb1f1427a394cf7d0d089feea6604d31d1f87162d5d870"
-  elsif OS.linux?
-    url "https://github.com/brandonkal/inkjet/releases/download/v0.12.0/inkjet-linux.tar.gz"
-    sha256 "1fb6566c203db2907aa35cd68ee3a7725a53961c26e1b45c29402aaa14274ff7"
+  on_macos do
+    on_arm do
+      url "https://github.com/brandonkal/inkjet/releases/download/untagged-28e054c9cf0e994b0494/inkjet-v0.15.0-aarch64-apple-darwin.tar.gz"
+      sha256 "1fc210eae8ce1d35b4db3a144e0c554120942376e639d47dcb0c8b17bf43d518"
+    end
+    on_intel do
+      url "https://github.com/brandonkal/inkjet/releases/download/untagged-28e054c9cf0e994b0494/inkjet-v0.15.0-x86_64-apple-darwin.tar.gz"
+      sha256 "ebbae940a933b49ac7e0b267d710c6b0d3255afbee94c91c0e689d901b1d7ab2"
+    end
   end
 
-  depends_on :arch => :x86_64
+  resource "completions" do
+    url "https://github.com/brandonkal/inkjet/releases/download/untagged-c99d005c4ff3882ef721/completions.tar.gz"
+    sha256 "b99d3d9a0cd8b169ee69926e123419f9949d2b4953bfe9c49930417cb3f4c258"
+  end
 
   def install
     bin.install "inkjet"
+
+    resource("completions").stage do
+      bash_completion.install "inkjet.bash" => "inkjet.bash"
+      fish_completion.install "inkjet.fish" => "inkjet.fish"
+    end
   end
 
   test do
